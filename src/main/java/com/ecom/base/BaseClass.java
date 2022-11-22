c7029e18e1b843cbbd3a6a89ed4dc44654c8ac16
Shreyas Joshi <kit.shraysjoshi@gmail.com>
2022-11-22 09:17:59 +0530

    DGTL-100: new banking automation web

---
+++ src/main/java/com/ecom/base/BaseClass.java
@@ -0,0 +1,20 @@
+package com.ecom.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+	public static String projectPath = System.getProperty("user.dir");
+	public static WebDriver driver;
+	
+	public static WebDriver getDriver() {
+		System.setProperty("webdriver.chrome.driver", projectPath+"\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();   //new FirefoxDriver(); new EdgeDriver();
+		driver.get("https://demo.guru99.com/V1/index.php");
+		return driver;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/ecom/testcases/LoginTest.java
@@ -0,0 +1,10 @@
+package com.ecom.testcases;
+
+public class LoginTest {
+	
+	public void testLogin() {
+		
+		
+	}
+
+}
